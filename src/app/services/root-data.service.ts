9fc7e8d160dee281da317dad44b33749151114dc
GaneshKollati98 <ganeshkollatii@gmail.com>
2022-05-05 10:49:28 +0530

    Projects: 1.Added new react expenses project to project component with demo video 2. For every project added technologies which ic used for projects 3. Condationally adding liveURL icon for projects 4. And Modified rootDateFile

--- src/app/services/root-data.service.ts
+++ src/app/services/root-data.service.ts
@@ -13,9 +13,9 @@ export class RootDataService {
       projects: 'fa fa-diagram-project',
       contact: 'fa fa-address-card',
     },
-    getInTouchMediaLinks:{
-       linkedIn:'https://www.linkedin.com/in/ganesh-kollati-2b238818b/',
-       github:'https://github.com/GaneshKollatii98'
+    getInTouchMediaLinks: {
+      linkedIn: 'https://www.linkedin.com/in/ganesh-kollati-2b238818b/',
+      github: 'https://github.com/GaneshKollatii98',
     },
     profiles: [
       '../../../../assets/profiles/linkedin.svg',
@@ -59,157 +59,203 @@ export class RootDataService {
         },
       ],
     },
-    experience:{
-      title:"Experience",
-      timelineElements : [
+    experience: {
+      title: 'Experience',
+      timelineElements: [
         {
           id: 1,
-          company:"Epam Systems",
-          title: "Software Engineer ",
-          location: "Hyderabad",
-          description:
-            `: Worked on two projects 1) Pizza ordering web app and Tech Stack is Frontend:- AngularJS, Bootstrap and Backend:- GraphQL, PostgreSQL, Sequelize. 
+          company: 'Epam Systems',
+          title: 'Software Engineer ',
+          location: 'Hyderabad',
+          description: `: Worked on two projects 1) Pizza ordering web app and Tech Stack is Frontend:- AngularJS, Bootstrap and Backend:- GraphQL, PostgreSQL, Sequelize. 
             2. Banking application all crud operations Tech Stack is Front End : AngularJS,Bootstrap`,
-          
-          date: "January 2022 - present",
-          icon: "laptop",
-          type:"Work"
+
+          date: 'January 2022 - present',
+          icon: 'laptop',
+          type: 'Work',
         },
         {
           id: 2,
-          company:"Infosys",
-          title: "System Engineer",
-          location: "Hyderabad",
+          company: 'Infosys',
+          title: 'System Engineer',
+          location: 'Hyderabad',
           description:
-            ": Worked on front end application Stack :- HTML, css, javascript",
-          date: "November 2020 - December 2021",
-          icon: "laptop",
-          type:"Work"
-          
+            ': Worked on front end application Stack :- HTML, css, javascript',
+          date: 'November 2020 - December 2021',
+          icon: 'laptop',
+          type: 'Work',
         },
         {
           id: 3,
-          title: "ITM University",
-          location: "Gwalior, Madya Pradesh",
-          degree:"Bachelors of computer science engineering",
-          description:
-        "",
-          date: "2016 - 2020 Passed Out",
-          icon: "graduation-cap",
-          
-          type:"University"
+          title: 'ITM University',
+          location: 'Gwalior, Madya Pradesh',
+          degree: 'Bachelors of computer science engineering',
+          description: '',
+          date: '2016 - 2020 Passed Out',
+          icon: 'graduation-cap',
+
+          type: 'University',
         },
         {
           id: 4,
-          title: "Narayana Juniour Collage",
-          location: "Bhimavaram, Andhra Pradesh",
-          degree:"Mathematics and physics,Chemistry",
-          
-          description:
-            "",
-          date: "2014 - 2016",
-          icon: "school",
-          type:"High School"
+          title: 'Narayana Juniour Collage',
+          location: 'Bhimavaram, Andhra Pradesh',
+          degree: 'Mathematics and physics,Chemistry',
+
+          description: '',
+          date: '2014 - 2016',
+          icon: 'school',
+          type: 'High School',
         },
         {
           id: 5,
           title: "St Mary's English Medium School",
-          location: "Bhimavaram, Andhra Pradesh",
-          degree:"Schooling",
-          
-          description:
-            "",
-          date: "2013 - 2014",
-          icon: "school-flag",
-          type:"School"
+          location: 'Bhimavaram, Andhra Pradesh',
+          degree: 'Schooling',
+
+          description: '',
+          date: '2013 - 2014',
+          icon: 'school-flag',
+          type: 'School',
         },
-      ]
-      
+      ],
     },
-    projects:{
-      title:"Projects",
-      projectsList:[
+    projects: {
+      title: 'Projects',
+      projectsList: [
         {
-          title:"Pizza Man",
-          experienceType:"Personal Project",
-          description:"Pizza Man is always open to serve you. Order all your favorite pizzas from the comfort of your home, and we will ensure free delivery for all orders. The tech stack consisted of Angular,Nodejs Express,Sequlize.",
-          sourceCodeURL:"https://github.com/Ganeshkollatii98/MyAnatomy-Training-Project/tree/master/Pizza-Man",
-          liveURL:"",
-          images:[
+          title: 'Pizza Man',
+          experienceType: 'Personal Project',
+          description:
+            'Pizza Man is always open to serve you. Order all your favorite pizzas from the comfort of your home, and we will ensure free delivery for all orders.',
+          techStack:'Angular, BootStrap, Nodejs,Express ,Sequlize.',
+          sourceCodeURL:
+            'https://github.com/Ganeshkollatii98/MyAnatomy-Training-Project/tree/master/Pizza-Man',
+          liveURL: '',
+          images: [
             {
-              type:"image",
-              imageUrl:"../../../../assets/projects/pizzaman/Home.PNG",
-              imageAlt:"Home Page"
+              type: 'image',
+              imageUrl: '../../../../assets/projects/pizzaman/Home.PNG',
+              imageAlt: 'Home Page',
             },
             {
-              type:"image",
-              imageUrl:"../../../../assets/projects/pizzaman/loginPage.PNG",
-              imageAlt:"Login Page"
+              type: 'image',
+              imageUrl: '../../../../assets/projects/pizzaman/loginPage.PNG',
+              imageAlt: 'Login Page',
             },
             {
-              type:"image",
-              imageUrl:"../../../../assets/projects/pizzaman/RigisterPage.PNG",
-              imageAlt:"Registration Page"
+              type: 'image',
+              imageUrl: '../../../../assets/projects/pizzaman/RigisterPage.PNG',
+              imageAlt: 'Registration Page',
             },
             {
-              type:"image",
-              imageUrl:"../../../../assets/projects/pizzaman/menu.PNG",
-              imageAlt:"Menu Page"
+              type: 'image',
+              imageUrl: '../../../../assets/projects/pizzaman/menu.PNG',
+              imageAlt: 'Menu Page',
             },
             {
-              type:"image",
-              imageUrl:"../../../../assets/projects/pizzaman/menuFilter.PNG",
-              imageAlt:"Menu Page Filters"
+              type: 'image',
+              imageUrl: '../../../../assets/projects/pizzaman/menuFilter.PNG',
+              imageAlt: 'Menu Page Filters',
             },
             {
-              type:"image",
-              imageUrl:"../../../../assets/projects/pizzaman/cartEmpty.PNG",
-              imageAlt:"Cart Page Empty"
+              type: 'image',
+              imageUrl: '../../../../assets/projects/pizzaman/cartEmpty.PNG',
+              imageAlt: 'Cart Page Empty',
             },
             {
-              type:"image",
-              imageUrl:"../../../../assets/projects/pizzaman/cartFilled.PNG",
-              imageAlt:"Cart Page Filled"
+              type: 'image',
+              imageUrl: '../../../../assets/projects/pizzaman/cartFilled.PNG',
+              imageAlt: 'Cart Page Filled',
             },
             {
-              type:"image",
-              imageUrl:"../../../../assets/projects/pizzaman/CheckOutBTN.PNG",
-              imageAlt:"Checkout Page"
+              type: 'image',
+              imageUrl: '../../../../assets/projects/pizzaman/CheckOutBTN.PNG',
+              imageAlt: 'Checkout Page',
             },
             {
-              type:"image",
-              imageUrl:"../../../../assets/projects/pizzaman/successPage.PNG",
-              imageAlt:"Success Page"
+              type: 'image',
+              imageUrl: '../../../../assets/projects/pizzaman/successPage.PNG',
+              imageAlt: 'Success Page',
             },
             {
-              type:"image",
-              imageUrl:"../../../../assets/projects/pizzaman/ordersList.PNG",
-              imageAlt:"Orders Page"
+              type: 'image',
+              imageUrl: '../../../../assets/projects/pizzaman/ordersList.PNG',
+              imageAlt: 'Orders Page',
             },
             {
-              video:
-              {
-              type:"video",
-              videoUrl:"../../../../assets/projects/pizzaman/sortVideo.mp4",
-              videoAlt:"Pizza Man Short Video"
-            }
-          },
+              video: {
+                type: 'video',
+                videoUrl: '../../../../assets/projects/pizzaman/sortVideo.mp4',
+                videoAlt: 'Pizza Man Short Video',
+              },
+            },
           ],
-        }
-        // Add New Projects here        
-      ]
+        },
+        {
+          title: 'Save Your Expenses',
+          experienceType: 'Personal Project',
+          description:
+            'Save your expenses, with the help of this application we can easliy track our daily expenses for every year.',
+          techStack:'React,css',
+          sourceCodeURL:
+            'https://github.com/Ganeshkollatii98/new.reactexpensechart.com',
+          liveURL: 'https://saveexpenses.netlify.com',
+          images: [
+            {
+              type: 'image',
+              imageUrl: '../../../../assets/projects/expenses/1.PNG',
+              imageAlt: 'Home Page',
+            },
+            {
+              type: 'image',
+              imageUrl: '../../../../assets/projects/expenses/2.PNG',
+              imageAlt: 'Login Page',
+            },
+            {
+              type: 'image',
+              imageUrl: '../../../../assets/projects/expenses/3.PNG',
+              imageAlt: 'Registration Page',
+            },
+            {
+              type: 'image',
+              imageUrl: '../../../../assets/projects/expenses/4.PNG',
+              imageAlt: 'Menu Page',
+            },
+            {
+              type: 'image',
+              imageUrl: '../../../../assets/projects/expenses/5.PNG',
+              imageAlt: 'Menu Page Filters',
+            },
+            {
+              type: 'image',
+              imageUrl: '../../../../assets/projects/expenses/6.PNG',
+              imageAlt: 'Cart Page Empty',
+            },
+            {
+              video: {
+                type: 'video',
+                videoUrl:
+                  '../../../../assets/projects/expenses/saveexpenses.mp4',
+                videoAlt: 'Save Expenses',
+              },
+            },
+          ],
+        },
+        // Add New Projects here
+      ],
     },
-    contact:{
-      title:"Coffee With Me.",
-      message:"I am always excited to work on some awesome projects, message me and let's discuss over coffee."
+    contact: {
+      title: 'Coffee With Me.',
+      message:
+        "I am always excited to work on some awesome projects, message me and let's discuss over coffee.",
     },
-    footer:{
-        name:"Ganesh Kollati",
-        made_msg:"Made with ❤️ In India",
-        socialMediaTitle:"You can find me every where",
-        image:"../../../../assets/foot.jpg"
-    }
-    
+    footer: {
+      name: 'Ganesh Kollati',
+      made_msg: 'Made with ❤️ In India',
+      socialMediaTitle: 'You can find me every where',
+      image: '../../../../assets/foot.jpg',
+    },
   };
   constructor() {}
 }
